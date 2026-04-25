8b2952cfe66df602cae04ca4c487c25b55b48a70
jmuli-star <muli2917@gmail.com>
2026-04-25 11:33:08 +0300

    admin file error field

--- Backend/Harvest_yield/admin.py
+++ Backend/Harvest_yield/admin.py
@@ -46,7 +46,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'role', 'associated_institution', 'institution_name', 'is_staff', 'is_superuser'),
+            'fields': ('email', 'password','role', 'associated_institution', 'institution_name', 'is_staff', 'is_superuser'),
         }),
     )
 
